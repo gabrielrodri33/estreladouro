64d1a334348104ca3a2beeb33bd75feef1050c87
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-01-23 17:20:44 -0300

    alterando página inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <>
-    <h1>Home!</h1>
     <div>
-        <h2>Hello World!</h2>
+        <h2></h2>
     </div>
     </>
   )
