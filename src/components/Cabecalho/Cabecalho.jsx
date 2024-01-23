28bb56964008be9fd0b665c7f59371ed204d5141
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-01-23 16:50:52 -0300

    Adicionado insta e wpp

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -9,13 +9,23 @@ export default function Cabecalho() {
           <Image src="/img/logo.png" alt="logo" width={279} height={98}></Image>
         </li>
         <li>
-          <Link href="">NOSSOS SERVIÇOS</Link>
+          <Link href="https://www.instagram.com/estreladouromazzei/" className="botao">NOSSOS SERVIÇOS</Link>
         </li>
         <li>
-          <Link href="">NOSSA HISTÓRIA</Link>
+          <Link href="" className="botao">NOSSA HISTÓRIA</Link>
         </li>
         <li>
-          <Link href="">DEPOIMENTOS</Link>
+          <Link href="" className="botao">DEPOIMENTOS</Link>
+        </li>
+        <li>
+          <Link href='https://www.instagram.com/estreladouromazzei/' target="_blank">
+            <Image src="/img/instagram.png" alt="whatsapp" width={50} height={50}></Image>
+          </Link>
+        </li>
+        <li>
+          <Link href='https://www.instagram.com/estreladouromazzei/' target="_blank">
+            <Image src="/img/whatsapp.png" alt="whatsapp" width={50} height={50}></Image>
+          </Link>
         </li>
       </ul>
     </header>
