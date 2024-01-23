0261e183ac469822830a2434aaff58dce039cf98
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-01-23 15:43:15 -0300

    Itens do cabeçalho ok

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,10 +1,23 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Cabecalho() {
   return (
     <header className="cabecalho">
-      <Image src="/img/logo.png" alt="logo" width={700} height={500}></Image>
+      <ul>
+        <li>
+          <Image src="/img/logo.png" alt="logo" width={279} height={98}></Image>
+        </li>
+        <li>
+          <Link href="">NOSSOS SERVIÇOS</Link>
+        </li>
+        <li>
+          <Link href="">NOSSA HISTÓRIA</Link>
+        </li>
+        <li>
+          <Link href="">DEPOIMENTOS</Link>
+        </li>
+      </ul>
     </header>
   );
 }
