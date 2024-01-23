84ca1260197444702e44d4ae567a73ec1d6cae10
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-01-23 17:20:12 -0300

    criado rodape

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,9 @@
+export default function Rodape() {
+    return (
+      <footer className="rodape">
+          <div>
+              <p>Av. Mazzei, 1882 - Vila Nova Mazzei, São Paulo - SP - Tel.: (011) 2996-7740</p>
+          </div>
+      </footer>
+    )
+  }
