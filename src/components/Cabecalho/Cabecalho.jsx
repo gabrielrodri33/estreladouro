0252db5de617060b07b551b7881daaff95eb1f69
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-01-26 13:48:05 -0300

    trabalhando com insta e wpp separado

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -3,32 +3,64 @@ import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-    <header className="cabecalho">
+    <header className="cabecalho py-7 flex mx-auto">
       <ul>
         <li>
-          <Image className="h-auto xl:w-64 lg:w-60 md:w-56 sm:w-48 2sm:w-40 3sm:w-32" src="/img/logo.png" alt="logo" width={270} height={87}></Image>
+          <Image
+            className="h-auto w-44 xl:w-64 lg:w-60 md:w-56 sm:w-52 2sm:w-48 3sm:w-44"
+            src="/img/logo.png"
+            alt="logo"
+            width={270}
+            height={87}
+          ></Image>
         </li>
         <li>
-          <Link href="#nossos_servicos" className="botao">SERVIÇOS</Link>
-        </li>
-        <li>
-          <Link href="#nossa_historia" className="botao">HISTÓRIA</Link>
-        </li>
-        <li>
-          <Link href="#depoimentos" className="botao">DEPOIMENTOS</Link>
-        </li>
-        <li>
-          <Link href='https://www.instagram.com/estreladouromazzei/' target="_blank">
-            <Image src="/img/instagram.png" alt="whatsapp" width={50} height={50}></Image>
+          <Link href="#nossos_servicos" className="botao">
+            SERVIÇOS
           </Link>
         </li>
         <li>
-          <Link href='https://www.instagram.com/estreladouromazzei/' target="_blank">
-            <Image src="/img/whatsapp.png" alt="whatsapp" width={50} height={50}></Image>
+          <Link href="#nossa_historia" className="botao">
+            HISTÓRIA
+          </Link>
+        </li>
+        <li>
+          <Link href="#depoimentos" className="botao">
+            DEPOIMENTOS
           </Link>
         </li>
       </ul>
-      <hr className="separator"/>
+
+      <ul>
+        <li>
+          <Link
+            href="https://www.instagram.com/estreladouromazzei/"
+            target="_blank"
+          >
+            <Image
+              src="/img/instagram.png"
+              alt="whatsapp"
+              width={50}
+              height={50}
+            ></Image>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://www.instagram.com/estreladouromazzei/"
+            target="_blank"
+          >
+            <Image
+              src="/img/whatsapp.png"
+              alt="whatsapp"
+              width={50}
+              height={50}
+            ></Image>
+          </Link>
+        </li>
+      </ul>
+
+      <hr className="separator" />
     </header>
   );
 }
