4cd682edd58e688af6ab94cc535b180e925ba111
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-01-26 16:18:07 -0300

    responsividade do cabeçalho completa

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-    <header className="bg-zinc-950 pt-5 md:font-bold text-center sticky top-0 z-20 py-7">
+    <header className="bg-zinc-950 md:font-bold text-center sticky top-0 z-20 pt-7">
       <div className="flex pb-5">
         <Image
-          className="justify-start h-auto w-44 xl:w-64 lg:w-60 md:w-56 sm:w-52 2sm:w-48 3sm:w-44"
+          className="justify-start h-auto w-28 xl:w-64 lg:w-60 md:w-56 sm:w-48 2sm:w-48 3sm:w-36 4sm:w-32"
           src="/img/logo.png"
           alt="logo"
           width={270}
           height={87}
         ></Image>
-        <div className="flex flex-row justify-center items-center" style={{ flex: 1 }}>
+        <div className="flex-row justify-center items-center hidden md:flex" style={{ flex: 1 }}>
           <ul className="flex md:flex-row flex-col text-xs md:text-lg md:items-center xl:space-x-52 lg:space-x-36 md:space-x-12 space-x-0 resize-x">
             <li>
               <Link href="#nossos_servicos" className="botao">
@@ -38,7 +38,7 @@ export default function Cabecalho() {
               target="_blank"
             >
               <Image
-                className="h-auto w-12 resize-none"
+                className="h-auto md:w-12 w-9 resize-none"
                 src="/img/instagram.png"
                 alt="whatsapp"
                 width={50}
@@ -52,7 +52,7 @@ export default function Cabecalho() {
               target="_blank"
             >
               <Image
-                className="h-auto w-12 resize-none"
+                className="h-auto md:w-12 w-9 resize-none"
                 src="/img/whatsapp.png"
                 alt="whatsapp"
                 width={50}
